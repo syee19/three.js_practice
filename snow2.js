bcff9a92209e8aef5cf98d600ea85283f26951e5
syee19 <syee19@naver.com>
2022-08-25 02:06:44 +0900

    Resize canvas

--- snow2.js
+++ snow2.js
@@ -278,7 +278,7 @@ function slideDirection(arr, dir) {
 function resizeRendererToDisplaySize(renderer) {
   const canvas = renderer.domElement;
   const width = canvas.clientWidth;
-  const height = canvas.clientWidth;
+  const height = width * 1.5; //canvas.clientWidth;
   const needResize = canvas.width !== width || canvas.height !== height;
   if (needResize) {
     renderer.setSize(width, height, false);
